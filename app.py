f783cfde659020541c6051c8112b7372ca3895a9
Sampath-KumarK <sampathkumark005@gmail.com>
2026-09-09 22:01:30 +0530

    After complete chunking and start work on embedding

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 import streamlit as st
+
 from utils.pdf_loader import load_pdf
+from utils.chunker import split_text
 
-st.title("📄 PDF Reader Test")
+st.title("📄 PDF Chunking Test")
 
-if st.button("Read PDF"):
+if st.button("Create Chunks"):
 
     pdf_text = load_pdf("data/medical.pdf")
 
-    st.success("PDF Loaded Successfully!")
+    chunks = split_text(pdf_text)
 
-    st.text_area(
-        "Extracted Text",
-        pdf_text,
-        height=400
-    )
+    st.success(f"Total Chunks Created: {len(chunks)}")
+
+    for i, chunk in enumerate(chunks):
+
+        with st.expander(f"Chunk {i+1}"):
+
+            st.write(chunk)
---
+++ utils/embedding.py
@@ -0,0 +1,14 @@
+from sentence_transformers import SentenceTransformer
+
+# Load embedding model only once
+embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
+
+
+def create_embeddings(chunks):
+    """
+    Convert text chunks into vector embeddings.
+    """
+
+    embeddings = embedding_model.encode(chunks)
+
+    return embeddings
